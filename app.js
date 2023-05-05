04dba313a99baee29fca39042b13085c1d37bb9c
Nageen Yousuf <homayonyousaf@gmail.com>
2023-05-05 20:41:02 +0500

    JS first assignment

---
+++ app.js
@@ -0,0 +1,101 @@
+//CHAPTER # 1//
+
+//-------Question no: 1 --------//
+//let user = "Welcome to the website! "
+//alert(user);
+
+
+//------Question no : 2 --------//
+//let user = "Error! please enter a valid password.";
+//document.write(user);
+
+
+//-------Question no :3-------//
+//let web = "alert!Welcome to JS land";
+//document.write(web);
+
+
+//-------Question no : 3------//
+//alert("Welcome to JS world!.....\Happy coding!");
+
+
+//------Question no : 4------//
+//alert("Welcome to javascript land!");
+//alert("Happy coding!....\n prevent this page from creating additional dialogues")
+
+
+//--------Question no : 5 ----//
+//alert("Hello!.....I  can run javascript through my web browser's console")
+
+        //CHAPTER # 2//
+
+//-----Question no : 1----//
+//declare a variable called username
+
+//let userName = "Nageen";
+//alert(userName);
+
+//------Question no : 2----//
+//declare a variable called myName and assign it to a string
+
+//let myName = "Nageen";
+//alert(myName);
+//console.log(myName);
+
+//----Question no : 3----//
+// declare a variable titled message in alert box
+
+//let message = "Hello World";
+//alert(message);
+
+//----Question no : 4-----//
+//declare a script to save student's bio data in JS variable and show in alert box 
+
+//let studentName = "Jhone Doe ";
+//let studentAge = "15 years old";
+//let studentEducation = "Certified Mobile Application Development";
+
+//alert(studentName);
+//alert(studentAge);
+//alert(studentEducation);
+
+//-----Question no : 5 -----//
+//write a script to display the following alert using one js variable
+
+//let abc = "PIZZA\n PIZZ\n PIZ\n PI\n P";
+//alert(abc);
+
+//-----Question no : 6----//
+//declare a variable called email 
+// use string concatenation
+//let email = "My email address is ";
+//let email1 ="homayonyousaf@gmail.com";
+//let email2 = email + email1;
+//alert(email2);
+
+
+//----Question no : 7 ----//
+//declare a variable called book and give it the value and display in alert box
+//let book = "I am trying to learn from the book A smarter way to learn JavaScript";
+//alert(book);
+
+//-----Question no : 7 ----//
+//write a script to display this in browser through JS
+//let write = "Yah! I can write HTML content through JavaScript";
+//document.write(write);
+
+
+//----Question no : 8 -----///
+//create a pattern in alert box
+
+//let pattern = "===$$~$$~$$===";
+//alert(pattern);
+
+
+
+
+
+
+
+
+
